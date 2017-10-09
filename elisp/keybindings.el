988c4d932c6e0835de7a29da89d77abf92a1db09
shivnshu <shivanshusinghraj@gmail.com>
2017-10-10 00:04:41 +0530

    Add multi edit and resize windows keybindings

--- elisp/keybindings.el
+++ elisp/keybindings.el
@@ -7,5 +7,14 @@
 (define-key evil-insert-state-map (kbd "C-v") 'cua-paste)
 (define-key evil-insert-state-map (kbd "C-x") 'cua-cut-region)
 
+;; Resize windows
+(global-set-key (kbd "C-x <up>") 'shrink-window)
+(global-set-key (kbd "C-x <down>") 'enlarge-window)
+(global-set-key (kbd "C-x <left>") 'shrink-window-horizontally)
+(global-set-key (kbd "C-x <right>") 'enlarge-window-horizontally)
+
+;; Other-window is M-o
+(global-set-key "\M-o" 'other-window)
+
 (provide 'keybindings)
 ;;; keybindings.el ends here
